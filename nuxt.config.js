cd83fb76119b8cdafab9760b93f95ae0af093c6d
rimduhui <>
2018-06-22 10:39:52 +0900

    add og

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,13 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '슬아 생일선물 대작전' }
+      { hid: 'description', name: 'description', content: '슬아 생일선물 대작전' },
+      { property: 'fb:app_id', content: '204228190210240' },
+      { property: 'og:url', content: 'http://bit.ly/2lpdN4v' },
+      { property: 'og:title', content: '슬아 생일선물 대작전' },
+      { property: 'og:image', content: 'https://png.pngtree.com/element_origin_min_pic/16/11/24/3f4a9b121c5769d1103ace681669b914.jpg' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:description', content: '그동안 슬아에게 못한 말을 생일선물로 주세요.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
--- pages/index.vue
+++ pages/index.vue
@@ -72,7 +72,7 @@ export default {
     return {
       title: '슬아 생축',
       meta: [
-        { property: 'fb:app_id', content: '204228190210240'},
+        { property: 'fb:app_id', content: '204228190210240' },
         { property: 'og:url', content: 'http://bit.ly/2lpdN4v' },
         { property: 'og:title', content: '슬아 생일선물 대작전' },
         { property: 'og:image', content: 'https://png.pngtree.com/element_origin_min_pic/16/11/24/3f4a9b121c5769d1103ace681669b914.jpg' },
