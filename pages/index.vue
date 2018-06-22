5548f2197ac3cc355422a657bc310da3e3e4ffb5
rimduhui <>
2018-06-22 10:34:18 +0900

    change image

--- pages/index.vue
+++ pages/index.vue
@@ -75,7 +75,7 @@ export default {
         { property: 'fb:app_id', content: '204228190210240'},
         { property: 'og:url', content: 'http://bit.ly/2lpdN4v' },
         { property: 'og:title', content: '슬아 생일선물 대작전' },
-        { property: 'og:image', content: '~/assets/img/seula_duhui.png' },
+        { property: 'og:image', content: 'https://png.pngtree.com/element_origin_min_pic/16/11/24/3f4a9b121c5769d1103ace681669b914.jpg' },
         { property: 'og:type', content: 'website' },
         { property: 'og:description', content: '그동안 슬아에게 못한 말을 생일선물로 주세요.' }
       ]
